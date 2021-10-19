5284fba4545be67e01c36af9749dfd379fd072ce
ijen <48159633+theyij@users.noreply.github.com>
2021-10-19 17:45:17 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+//test colors
+let colors = ["#b28fce", "#336774", "#ab3b3a", "#6a8372", "#86c166", "#d9cd90", "#f75c2f"];
+
+//target btn
+let btn = document.getElementById("btn");
+let color = document.querySelector(".color");
+let nav = document.querySelectorAll(".chg");
+
+function getRandom(){
+    //get random num for selecting colors array
+
+    return Math.floor(Math.random()* colors.length);
+}
+
+btn.addEventListener("click", function(){
+    //change background with each click
+
+    const randomNum = getRandom();
+
+    document.body.style.backgroundColor = colors[randomNum];
+    for(let item of nav){
+        item.style.color = colors[randomNum];
+    }
+    color.textContent = colors[randomNum];
+
+})
